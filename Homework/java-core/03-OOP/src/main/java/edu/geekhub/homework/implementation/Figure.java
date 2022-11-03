820d1d7360e2477e878b439592a7152777e40e46
Konstantion <yagudinkostya@gmail.com>
2022-11-03 21:22:38 +0200

    L03 Added abstract class Figure.

---
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/implementation/Figure.java
@@ -0,0 +1,11 @@
+package edu.geekhub.homework.implementation;
+
+import edu.geekhub.homework.interfaces.Colorable;
+
+
+
+public abstract class Figure implements Colorable {
+    public abstract double getPerimeter();
+
+    public abstract double getArea();
+}
