06aeecb0dab8417bb6b45d4ce0093ab93e1dc72b
Konstantion <yagudinkostya@gmail.com>
2022-11-04 21:40:38 +0200

    L03 Changed message in WrongInputMessages

--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/messages/WrongInputMessages.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/messages/WrongInputMessages.java
@@ -1,7 +1,7 @@
 package edu.geekhub.homework.utilities.messages;
 
 public class WrongInputMessages {
-    public static final String MUST_BE_GREATER_THAN_ZERO = "Side/Radius must be greater that zero";
+    public static final String MUST_BE_GREATER_THAN_ZERO = "Side/Radius length must be greater that zero";
     public static final String MUST_BE_GREATER_THAT_THIRD = "Sum of two sides of a triangle"
             + " must be greater than the third side";
     public static final String NO_SUCH_COLOR = "No such color provided";
