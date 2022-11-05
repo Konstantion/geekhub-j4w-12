598d71e90e213a8f2a2d0dfaa1eeb84cd5e1f340
Konstantion <yagudinkostya@gmail.com>
2022-11-05 23:36:49 +0200

    L03 Added field parameter to comparators.

--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/figures/comparators/AreaComparator.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/figures/comparators/AreaComparator.java
@@ -3,6 +3,8 @@ package edu.geekhub.homework.figures.comparators;
 import edu.geekhub.homework.implementation.Figure;
 
 public class AreaComparator implements FigureComparator {
+    public final String parameter = "area";
+
     @Override
     public int compare(Figure f1, Figure f2) {
         return Double.compare(f1.getArea(), f2.getArea());
--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/figures/comparators/PerimeterComparator.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/figures/comparators/PerimeterComparator.java
@@ -1,9 +1,10 @@
 package edu.geekhub.homework.figures.comparators;
 
 import edu.geekhub.homework.implementation.Figure;
-import java.util.Comparator;
 
 public class PerimeterComparator implements FigureComparator {
+    public final String parameter = "perimeter";
+
     @Override
     public int compare(Figure f1, Figure f2) {
         return Double.compare(f1.getPerimeter(), f2.getPerimeter());
