184143f8a616308e212851225fcdbb0bafbcae38
Konstantion <yagudinkostya@gmail.com>
2022-11-05 20:41:30 +0200

    L03 Extended class FigureInputMessages, added successfully created message, and method to get message to input sides

--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/messages/FigureInputMessages.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/messages/FigureInputMessages.java
@@ -1,6 +1,8 @@
 package edu.geekhub.homework.utilities.messages;
 
 
+import static edu.geekhub.homework.utilities.messages.MessagesColors.*;
+
 public class FigureInputMessages {
     public static final String SQUARE = "square";
     public static final String TRIANGLE = "triangle";
@@ -10,6 +12,17 @@ public class FigureInputMessages {
     public static final String CIRCLE_INPUT = "circle radius length";
     public static final String RECTANGLE_INPUT = "rectangle sides length in separated lines";
     public static final String TRIANGLE_INPUT = "triangle sides length in separated lines";
+    public static final String SUCCESSFULLY_CREATED = ANSI_GREEN
+                            + "Figure successfully created"
+                            + ANSI_RESET;
+    public static String getSideMessage(int index){
+        return String.format(
+                ANSI_YELLOW
+                + "Enter %d side"
+                + ANSI_RESET,
+                index
+        );
+    }
 
 
     private FigureInputMessages() {
