35d4ba8d3dee759e082f038193fea72d4bfa920c
Konstantion <yagudinkostya@gmail.com>
2022-11-05 20:13:02 +0200

    L03 Added figure input messages

---
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/messages/FigureInputMessages.java
@@ -0,0 +1,18 @@
+package edu.geekhub.homework.utilities.messages;
+
+
+public class FigureInputMessages {
+    public static final String SQUARE = "square";
+    public static final String TRIANGLE = "triangle";
+    public static final String RECTANGLE = "rectangle";
+    public static final String CIRCLE = "circle";
+    public static final String SQUARE_INPUT = "square side length";
+    public static final String CIRCLE_INPUT = "circle radius length";
+    public static final String RECTANGLE_INPUT = "rectangle sides length in separated lines";
+    public static final String TRIANGLE_INPUT = "triangle sides length in separated lines";
+
+
+    private FigureInputMessages() {
+
+    }
+}
