8c2af7f4c22818291eacd10dfc54bc6bae822ca5
Konstantion <yagudinkostya@gmail.com>
2022-11-05 23:49:50 +0200

    L03 fixed bug in StringUtilities.

--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/string/StringUtilities.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/string/StringUtilities.java
@@ -2,7 +2,7 @@ package edu.geekhub.homework.utilities.string;
 
 public class StringUtilities {
     public static String center(String text, int len) {
-        String out = String.format("%" + "%d" + "s%s%" + "d" + "s", len, "", text, len, "");
+        String out = String.format("%" + len + "s%s%" + len + "s", "", text, "");
         float mid = (out.length() / 2);
         float start = mid - (len / 2);
         float end = start + len;
