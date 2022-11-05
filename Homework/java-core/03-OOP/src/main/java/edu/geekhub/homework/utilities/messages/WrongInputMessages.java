7d5f80e300b4fdf8228cff49f788c60b27cd6f41
Konstantion <yagudinkostya@gmail.com>
2022-11-05 20:39:19 +0200

    L03 Message colors extracted to own class

--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/messages/WrongInputMessages.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/messages/WrongInputMessages.java
@@ -1,16 +1,6 @@
 package edu.geekhub.homework.utilities.messages;
 
 public class WrongInputMessages {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
     public static final String MUST_BE_GREATER_THAN_ZERO = "Side/Radius length must be greater that zero";
     public static final String MUST_BE_GREATER_THAT_THIRD = "Sum of two sides of a triangle"
             + " must be greater than the third side";
