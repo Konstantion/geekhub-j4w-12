983c03c00b4d986bdaa24962fd8c7d4b55c96a8c
Konstantion <yagudinkostya@gmail.com>
2022-11-05 15:31:50 +0200

    L03 Work on user interaction interface.

--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/messages/UserInteractionMessages.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/messages/UserInteractionMessages.java
@@ -2,32 +2,41 @@ package edu.geekhub.homework.utilities.messages;
 
 import edu.geekhub.homework.figures.comparators.FigureComparator;
 import edu.geekhub.homework.implementation.Figure;
+
 import java.util.Comparator;
+
+import static edu.geekhub.homework.utilities.messages.WrongInputMessages.*;
 
 public class UserInteractionMessages {
     public static String selectShapeMessage(int index) {
         return String.format(
-                "You are selecting the %d shape:%n"
+                ANSI_YELLOW
+                + "You are selecting the %d shape:%n"
                 + "Press 1 if you want to select CIRCLE%n"
                 + "Press 2 if you want to select TRIANGLE%n"
                 + "Press 3 if you want to select SQUARE%n"
-                + "Press 4 if you want to select RECTANGLE%n",
+                + "Press 4 if you want to select RECTANGLE%n"
+                + ANSI_RESET,
                 index
         );
     }
 
     public static String wrongInputMessage(String message) {
         return String.format(
-                "Wrong input data: %S%n"
-                + "Try again!%n",
+                ANSI_RED
+                + "Wrong input data: %S%n"
+                + "Try again!%n"
+                + ANSI_RESET,
                 message
         );
     }
 
     public static String showFigureMessage(int index, Figure figure) {
         return String.format(
-                "figure %d:%n"
-                + "%s",
+                ANSI_BLUE
+                + "figure %d:%n"
+                + "%s"
+                + ANSI_RESET,
                 index,
                 figure.toString()
         );
@@ -35,15 +44,19 @@ public class UserInteractionMessages {
 
     public static String selectColorMessage() {
         return String.format(
-                "You are selecting color of the shape:%n"
+                ANSI_YELLOW
+                + "You are selecting color of the shape:%n"
                 + "Type the title of the color, or if you want it to be default press `ENTER`%n"
+                + ANSI_RESET
         );
     }
 
     public static String compareFiguresMessage(Figure figure1, Figure figure2,
                                                FigureComparator comparator, String parameter) {
         return String.format(
-                "figure1's %s is %s figure2's %s%n",
+                ANSI_PURPLE
+                + "figure1's %s is %s figure2's %s%n"
+                + ANSI_RESET,
                 parameter,
                 FigureComparator.convertComparatorResultToString(
                         comparator.compare(figure1, figure2)
@@ -51,7 +64,14 @@ public class UserInteractionMessages {
                 parameter
         );
     }
-
+    public static String userChoiceMessage(){
+        return String.format(
+                ANSI_YELLOW
+                + "You chose a %s%n"
+                + "Enter the %s, console will read only numeric value:%n"
+                + ANSI_RESET
+        );
+    }
     private UserInteractionMessages() {
 
     }
--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/response/handler/UserResponseHandler.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/response/handler/UserResponseHandler.java
@@ -1,11 +1,16 @@
 package edu.geekhub.homework.utilities.response.handler;
 
 import static edu.geekhub.homework.utilities.messages.WrongInputMessages.MUST_BE_GREATER_THAN_ZERO;
+import static edu.geekhub.homework.utilities.messages.WrongInputMessages.WRONG_INPUT_FORMAT;
 
+import edu.geekhub.homework.figures.Circle;
+import edu.geekhub.homework.figures.Rectangle;
 import edu.geekhub.homework.figures.Square;
+import edu.geekhub.homework.figures.Triangle;
 import edu.geekhub.homework.implementation.Figure;
 import edu.geekhub.homework.utilities.checkers.FigureInputChecker;
 import edu.geekhub.homework.utilities.messages.UserInteractionMessages;
+
 import java.util.Scanner;
 
 
@@ -18,20 +23,69 @@ public class UserResponseHandler {
     }
 
     public static Square generateSquare() {
-        System.out.printf("You chose a square%n"
-                + "Enter a length of the square side, console will read only numeric value: ");
+        System.out.printf(UserInteractionMessages.userChoiceMessage());
+        double side = getDoubleSideFromConsole();
+        System.out.printf("Successfully build square with side %f%n", side);
+        return Square.builder()
+                .side(side)
+                .build();
+    }
+
+    public static Circle generateCircle() {
+        System.out.printf(UserInteractionMessages.userChoiceMessage());
+        double radius = getDoubleSideFromConsole();
+        System.out.printf("Successfully build circle with radius %f%n", radius);
+        return Circle.builder()
+                .radius(radius)
+                .build();
+    }
+
+    public static Rectangle generateRectangle() {
+        System.out.printf(UserInteractionMessages.userChoiceMessage());
+        System.out.printf("Enter first side length:%n");
+        double height = getDoubleSideFromConsole();
+        System.out.printf("Enter second side length:%n");
+        double width = getDoubleSideFromConsole();
+        System.out.printf("Successfully build rectangle with sides %f %f%n", height, width);
+        return Rectangle.builder()
+                .width(width)
+                .height(height)
+                .build();
+    }
+
+    public static Triangle generateTriangle() {
+        System.out.printf(UserInteractionMessages.userChoiceMessage());
+        double sideA = getDoubleSideFromConsole();
+        double sideB = getDoubleSideFromConsole();
+        double sideC = getDoubleSideFromConsole();
+        System.out.printf("Successfully build triangle with sides %f %f %f%n", sideA, sideB, sideC);
+        return Triangle.builder()
+                .sideA(sideA)
+                .sideB(sideB)
+                .sideC(sideC)
+                .build();
+    }
+
+    private static double getDoubleSideFromConsole() {
         while (scanner.hasNext()) {
-            double side = scanner.nextDouble();
-            if (FigureInputChecker.isSideExist(side)) {
-                System.out.printf("Successfully build square with side %f%n", side);
-                return Square.builder()
-                        .side(side)
-                        .build();
+            if (scanner.hasNextDouble()) {
+                double radius = scanner.nextDouble();
+                if (FigureInputChecker.isSideExist(radius)) {
+                    scanner.nextLine();
+                    return radius;
+                } else {
+                    System.out.printf(UserInteractionMessages
+                            .wrongInputMessage(MUST_BE_GREATER_THAN_ZERO));
+                }
             } else {
-                System.out.println(UserInteractionMessages
-                        .wrongInputMessage(MUST_BE_GREATER_THAN_ZERO));
+                System.out.printf(UserInteractionMessages
+                        .wrongInputMessage(WRONG_INPUT_FORMAT));
+                scanner.next();
             }
         }
-        return null;
+        return Double.MIN_VALUE;
+    }
+
+    private UserResponseHandler() {
     }
 }
