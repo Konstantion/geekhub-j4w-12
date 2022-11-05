544654a4212fa47eb1a7c5835e46b81371d26c8b
Konstantion <yagudinkostya@gmail.com>
2022-11-05 09:53:39 +0200

    L03 Added user response handler, started to work on user interface.

--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,6 +1,15 @@
 package edu.geekhub.homework;
 
-public class ApplicationStarter {
+import edu.geekhub.homework.implementation.Figure;
+import edu.geekhub.homework.utilities.messages.UserInteractionMessages;
 
-    // Write code here :)
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        Figure figure1;
+        Figure figure2;
+        for (int i = 1; i <= 2; i++) {
+            System.out.println(UserInteractionMessages.selectShapeMessage(i));
+            //todo: finish user interface
+        }
+    }
 }
---
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/utilities/response/handler/UserResponseHandler.java
@@ -0,0 +1,21 @@
+package edu.geekhub.homework.utilities.response.handler;
+
+import edu.geekhub.homework.figures.Square;
+import edu.geekhub.homework.implementation.Figure;
+
+import java.util.Scanner;
+
+public class UserResponseHandler {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static Figure selectedFigureHandler(int index){
+        //todo: finish method
+        return null;
+    }
+    private static Square generateSquare(){
+        System.out.printf("You chose a square%n"
+                + "Enter a length of the square side, console will read only numeric value: ");
+        //todo: finish method
+        return null;
+    }
+}
