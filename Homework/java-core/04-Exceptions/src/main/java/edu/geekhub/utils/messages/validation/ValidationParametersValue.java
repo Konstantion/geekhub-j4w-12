988c968d4c0b375b8141847b5721989a99848474
Konstantion <yagudinkostya@gmail.com>
2022-11-11 12:47:36 +0200

    L04 Added validation parameters value.

---
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/utils/messages/validation/ValidationParametersValue.java
@@ -0,0 +1,18 @@
+package edu.geekhub.utils.messages.validation;
+
+public class ValidationParametersValue {
+    public static final String SPECIFIC_CHARACTERS = "(){}`/\\]['”";
+    public static final String USER_ID = "User ID";
+    public static final String USER_EMAIL = "User email";
+    public static final String USERNAME = "Username";
+    public static final String USER_FULL_NAME = "User full name";
+    public static final String USER_AGE = "User age";
+    public static final String MAX_AGE = "100";
+    public static final String MIN_AGE = "18";
+    public static final String USER_NOTES = "User notes";
+    public static final String NOTES_LENGTH = "255";
+    public static final String AMOUNT_OF_USER_FOLLOWERS = "Amount of user followers";
+
+    private ValidationParametersValue() {
+    }
+}
