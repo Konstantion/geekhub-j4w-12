587c4aa2162f98296328b4a311710fcf4818eff0
Konstantion <yagudinkostya@gmail.com>
2022-11-11 14:17:55 +0200

    L04 Changed validation parameters value class to enum.

---
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/utils/validation/messages/ValidationParameter.java
@@ -0,0 +1,26 @@
+package edu.geekhub.utils.validation.messages;
+
+public enum ValidationParameter {
+    SPECIFIC_CHARACTERS("(){}`/\\]['”"),
+    USER_ID("User ID"),
+    USER_EMAIL("User email"),
+    USERNAME("Username"),
+    USER_FULL_NAME("User full name"),
+    USER_AGE("User age"),
+    MAX_AGE("100"),
+    MIN_AGE("18"),
+    USER_NOTES("User notes"),
+    NOTES_LENGTH("255"),
+    AMOUNT_OF_USER_FOLLOWERS("Amount of user followers");
+
+    private String parameter;
+
+    ValidationParameter(String parameter) {
+        this.parameter = parameter;
+    }
+
+    public String getParameter() {
+        return parameter;
+    }
+
+}
