bc493f6845093b066a12ff9101425ceaf22f1d00
Konstantion <yagudinkostya@gmail.com>
2022-11-12 02:28:04 +0200

    L04 Working on my own Request generator to test the program.

--- Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,10 +1,14 @@
 package edu.geekhub.homework;
 
 import edu.geekhub.controller.Controller;
+import edu.geekhub.models.User;
 import edu.geekhub.models.request.Request;
 import edu.geekhub.models.request.Response;
 import edu.geekhub.utils.RandomRequestDataGenerator;
 import edu.geekhub.utils.RequestDataGenerator;
+import edu.geekhub.utils.generator.ObedientRequestGenerator;
+
+import java.util.UUID;
 
 // Don't move this class
 public class ApplicationStarter {
@@ -12,14 +16,31 @@ public class ApplicationStarter {
     private static final Controller controller = new UserController();
 
     private static final RequestDataGenerator generator = new RandomRequestDataGenerator();
+    private static final ObedientRequestGenerator obedientGenerator = new ObedientRequestGenerator();
 
     public static void main(String[] args) {
-        while(true) {
-            Request request = generator.generate();
+        obedientGenerator.VALUE_CAN_BE_NULL = false;
+        obedientGenerator.EMAIL_CAN_BE_INVALID = false;
+
+        int i = 0;
+
+        while (i < 100) {
+            User user = User.toBuilder()
+                    .withId(UUID.randomUUID())
+                    .withAge(20)
+                    .withAmountOfFollowers(20L)
+                    .withUserName("kostya")
+                    .withFullName("Kostya Yagudin")
+                    .withNotes("My note")
+                    .withEmail(obedientGenerator.generateEmail())
+                    .build();
+            Request request = new Request(user);
+            //Request request = generator.generate();
 
             Response response = controller.process(request);
 
             System.out.println(response);
+            i++;
         }
     }
 }
