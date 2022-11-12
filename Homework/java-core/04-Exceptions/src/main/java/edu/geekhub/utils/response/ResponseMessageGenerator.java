2bdea6d43f5babdc279a8eeab2e768d80decad05
Konstantion <yagudinkostya@gmail.com>
2022-11-12 15:30:22 +0200

    L04 Added new messages to ResponseMessageGenerator.

--- Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/utils/response/ResponseMessageGenerator.java
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/utils/response/ResponseMessageGenerator.java
@@ -6,7 +6,7 @@ import static edu.geekhub.utils.string.MessagesColors.*;
 
 public class ResponseMessageGenerator {
 
-    public static String successResponseMessage(User user) {
+    public static String successSaveUserResponseMessage(User user) {
         return String.format(
                 ANSI_GREEN
                         + "User have been successfully saved: "
@@ -16,12 +16,32 @@ public class ResponseMessageGenerator {
         );
     }
 
-    public static String failedResponseMessage(String exceptionMessage, Object data) {
+    public static String failedValidationResponseMessage(String exceptionMessage, Object data) {
         return String.format(
                 ANSI_RED
-                        + "User haven't been successfully saved, error occurs: "
+                        + "User haven't been successfully validated, error occurs: "
                         + ANSI_RESET
-                        + "[%s, %s]",
+                        + "[%s : %s]",
+                data.toString(),
+                exceptionMessage
+        );
+    }
+    public static String failedConnectToMemoryStorageResponseMessage(String exceptionMessage, Object data) {
+        return String.format(
+                ANSI_RED
+                        + "Failed to connect to memory storage, error occurs: "
+                        + ANSI_RESET
+                        + "[%s : %s]",
+                data.toString(),
+                exceptionMessage
+        );
+    }
+    public static String failedToCastDataToUser(String exceptionMessage, Object data) {
+        return String.format(
+                ANSI_RED
+                        + "Failed to case given data to User object: "
+                        + ANSI_RESET
+                        + "[%s : %s]",
                 data.toString(),
                 exceptionMessage
         );
