686523a6c417b224efb9bda0e68b73ef12874529
Konstantion <yagudinkostya@gmail.com>
2022-11-18 02:43:05 +0200

    L05 Added method validateInputPresent.

--- Homework/java-core/05-Unit-Testing/src/main/java/edu/geekhub/homework/task2/LosesInWarParser.java
+++ Homework/java-core/05-Unit-Testing/src/main/java/edu/geekhub/homework/task2/LosesInWarParser.java
@@ -1,11 +1,30 @@
 package edu.geekhub.homework.task2;
 
+
 import static edu.geekhub.homework.util.NotImplementedException.TODO_TYPE;
+import static java.util.Objects.requireNonNull;
 
 public class LosesInWarParser {
 
      LosesStatistic parseLosesStatistic(String input) {
+         validateInputPresent(input);
+         validateContainsStatistics(input);
+
         return TODO_TYPE();
+    }
+
+    private void validateContainsStatistics(String input) {
+         String[] statisticsLines = input.split(System.lineSeparator());
+    }
+
+    private void validateInputPresent(String input) {
+        requireNonNull(input);
+
+        if(input.isBlank()) {
+            throw new IllegalArgumentException(
+                    "Cant process empty input state"
+            );
+        }
     }
 
 }
