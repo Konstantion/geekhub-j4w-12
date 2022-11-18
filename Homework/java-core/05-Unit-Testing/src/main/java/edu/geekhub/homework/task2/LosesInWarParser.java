242aa6cf7b19615e758b6be8a84e2e291b5cde71
Konstantion <yagudinkostya@gmail.com>
2022-11-18 13:56:14 +0200

    L05 Added method to check if input contains statistics.

--- Homework/java-core/05-Unit-Testing/src/main/java/edu/geekhub/homework/task2/LosesInWarParser.java
+++ Homework/java-core/05-Unit-Testing/src/main/java/edu/geekhub/homework/task2/LosesInWarParser.java
@@ -5,22 +5,35 @@ import static edu.geekhub.homework.util.NotImplementedException.TODO_TYPE;
 import static java.util.Objects.requireNonNull;
 
 public class LosesInWarParser {
+    private static final String[] parameters = new String[]{
+            "Танки", "ББМ", "Гармати", "РСЗВ", "Засоби ППО",
+            "Літаки", "Гелікоптери", "БПЛА", "Крилаті ракети",
+            "Кораблі (катери)", "Cпeцiaльнa тeхнiкa", "Особовий склад"
+    };
 
-     LosesStatistic parseLosesStatistic(String input) {
-         validateInputPresent(input);
-         validateContainsStatistics(input);
+    LosesStatistic parseLosesStatistic(String input) {
+        validateInputPresent(input);
+
+        if(!containsStatistics(input)) {
+            return LosesStatistic.empty();
+        }
 
         return TODO_TYPE();
     }
 
-    private void validateContainsStatistics(String input) {
-         String[] statisticsLines = input.split(System.lineSeparator());
+    private boolean containsStatistics(String input) {
+        for (int i = 0; i < parameters.length; i++) {
+            if(!input.contains(parameters[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private void validateInputPresent(String input) {
         requireNonNull(input);
 
-        if(input.isBlank()) {
+        if (input.isBlank()) {
             throw new IllegalArgumentException(
                     "Cant process empty input state"
             );
