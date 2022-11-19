03619e92436dab870a8dc6477fcf423f041a79b8
Konstantion <yagudinkostya@gmail.com>
2022-11-20 00:31:59 +0200

    L05 Checks and bug fix.

--- Homework/java-core/05-Unit-Testing/src/test/java/edu/geekhub/homework/task3/utils/TriviaQueueImpTest.java
+++ Homework/java-core/05-Unit-Testing/src/test/java/edu/geekhub/homework/task3/utils/TriviaQueueImpTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TrivialQueueImpTest {
+class TriviaQueueImpTest {
 
     private TrivialStringQueue queue;
 
