66bccfbbea77144b658ab6956a9ac082ef2deffb
Konstantion <yagudinkostya@gmail.com>
2022-11-19 03:18:57 +0200

    L05 Added tests for future realisation of class.

---
+++ Homework/java-core/05-Unit-Testing/src/test/java/edu/geekhub/homework/task3/utils/TrivialStackImpTest.java
@@ -0,0 +1,74 @@
+package edu.geekhub.homework.task3.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrivialStackImpTest {
+
+    private TrivialStringStack stack;
+
+    @BeforeEach
+    void setUp() {
+        stack = new TrivialStackImp();
+    }
+
+    @Test
+    void Given_EmptyStack_When_GetSize_Than_ThrowsIllegalArgumentException() {
+        assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> stack.size()
+        );
+    }
+
+    @Test
+    void Given_EmptyStack_When_Pop_Than_ThrowsIllegalArgumentException() {
+        assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> stack.pop()
+        );
+    }
+
+    @Test
+    void Given_EmptyStack_When_Peak_Than_ThrowsIllegalArgumentException() {
+        assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> stack.peak()
+        );
+    }
+
+    @Test
+    void Given_StringAsInput_When_Push_Than_SizeReturnOne() {
+        stack.push("Some String");
+
+        assertEquals(1, stack.size());
+    }
+
+    @Test
+    void Given_StackWithTwoElements_When_Pop_Than_PeakReturnFirstElementAndSizeWillBeOne() {
+        String first = "FIRST";
+        String second = "SECOND";
+
+        stack.push(first);
+        stack.push(second);
+        String popString = stack.pop();
+        String peakString = stack.peak();
+
+        assertEquals(popString, second);
+        assertEquals(peakString, first);
+        assertEquals(1, stack.size());
+    }
+
+    @Test
+    @DisplayName("This test checks if stack automatically expands")
+    void Given_OneHundredTenElements_When_Push_Than_ReturnOneHundredTenElements() {
+        for(int i = 0; i < 110; i++) {
+            stack.push(String.valueOf(i));
+        }
+
+        assertEquals(110, stack.size());
+    }
+
+}
