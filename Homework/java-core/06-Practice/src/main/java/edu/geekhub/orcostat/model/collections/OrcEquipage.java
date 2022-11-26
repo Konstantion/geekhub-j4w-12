db85059581e63a86b6a54f238004c9379b6fb147
Konstantion <yagudinkostya@gmail.com>
2022-11-27 00:30:28 +0200

    L06 Added method to generate equipage

--- Homework/java-core/06-Practice/src/main/java/edu/geekhub/orcostat/model/collections/OrcEquipage.java
+++ Homework/java-core/06-Practice/src/main/java/edu/geekhub/orcostat/model/collections/OrcEquipage.java
@@ -4,11 +4,23 @@ import edu.geekhub.orcostat.model.entity.Orc;
 
 import java.security.SecureRandom;
 
-public class OrcEquipage extends TrivialOrcCollection{
+public class OrcEquipage extends TrivialOrcCollection {
     private static final SecureRandom random = new SecureRandom();
+
     public static TrivialOrcCollection generateRandomEquipage() {
         TrivialOrcCollection equipage = new TrivialOrcCollection();
-        for(int i = 0; i < random.nextInt(0,7); i++) {
+        for (int i = 0; i < random.nextInt(0, 7); i++) {
+            equipage.add(new Orc());
+        }
+        return equipage;
+    }
+
+    public static TrivialOrcCollection generateEquipage(int bound) {
+        if (bound < 0 || bound > 6) {
+            throw new IllegalArgumentException("Equipage size must be between 0 and 6");
+        }
+        TrivialOrcCollection equipage = new TrivialOrcCollection();
+        for (int i = 0; i < bound; i++) {
             equipage.add(new Orc());
         }
         return equipage;
