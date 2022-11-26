e76604e4264ab8a62fb0c523b05a514a2c3aa104
Konstantion <yagudinkostya@gmail.com>
2022-11-26 13:08:59 +0200

    L06 Added method to concat arrays and test for it

--- Homework/java-core/06-Practice/src/main/java/edu/geekhub/orcostat/model/utils/TrivialArrayUtil.java
+++ Homework/java-core/06-Practice/src/main/java/edu/geekhub/orcostat/model/utils/TrivialArrayUtil.java
@@ -2,11 +2,14 @@ package edu.geekhub.orcostat.model.utils;
 
 import edu.geekhub.orcostat.model.entity.MilitaryLoss;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class TrivialArrayUtil {
-    public static MilitaryLoss[] filterMilitaryLossByLocalDate(MilitaryLoss[] militaryLosses, LocalDate date) {
+    public static MilitaryLoss[] filterMilitaryLossByLocalDate(
+            MilitaryLoss[] militaryLosses, LocalDate date) {
         MilitaryLoss[] filteredArray = new MilitaryLoss[militaryLosses.length];
         int filteredIndex = 0;
 
@@ -18,5 +21,19 @@ public class TrivialArrayUtil {
         }
 
         return Arrays.copyOf(filteredArray, filteredIndex);
+    }
+
+    public static MilitaryLoss[] concatAll(MilitaryLoss[] first, MilitaryLoss[]... rest) {
+        int totalLength = first.length;
+        for (MilitaryLoss[] array : rest) {
+            totalLength += array.length;
+        }
+        MilitaryLoss[] result = Arrays.copyOf(first, totalLength);
+        int offset = first.length;
+        for (MilitaryLoss[] array : rest) {
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+        return result;
     }
 }
--- Homework/java-core/06-Practice/src/test/java/edu/geekhub/orcostat/model/utils/TrivialArrayUtilTest.java
+++ Homework/java-core/06-Practice/src/test/java/edu/geekhub/orcostat/model/utils/TrivialArrayUtilTest.java
@@ -1,7 +1,6 @@
 package edu.geekhub.orcostat.model.utils;
 
-import edu.geekhub.orcostat.model.entity.MilitaryLoss;
-import edu.geekhub.orcostat.model.entity.Orc;
+import edu.geekhub.orcostat.model.entity.*;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -14,7 +13,7 @@ class TrivialArrayUtilTest {
         Orc orc_1 = new Orc();
         Orc orc_2 = new Orc();
         Orc orc_3 = new Orc();
-        MilitaryLoss[] militaryLosses = new MilitaryLoss[] {
+        MilitaryLoss[] militaryLosses = new MilitaryLoss[]{
                 orc_1,
                 new Orc(LocalDate.parse("2022-02-10")),
                 orc_2,
@@ -24,13 +23,40 @@ class TrivialArrayUtilTest {
 
         MilitaryLoss[] actualFilteredArray = TrivialArrayUtil
                 .filterMilitaryLossByLocalDate(militaryLosses, LocalDate.now());
-        MilitaryLoss[] expectedFilteredArray = new MilitaryLoss[] {
+        MilitaryLoss[] expectedFilteredArray = new MilitaryLoss[]{
                 orc_1,
                 orc_2,
                 orc_3
         };
 
         assertArrayEquals(expectedFilteredArray, actualFilteredArray);
+    }
+
+    @Test
+    void concat_arrays_work() {
+        Orc orc_1 = new Orc();
+        Orc orc_2 = new Orc();
+        Tank tank_1 = new Tank();
+        Tank tank_2 = new Tank();
+        Drone drone_1 = new Drone();
+        Missile missile_1 = new Missile();
+        MilitaryLoss[] orcs = new MilitaryLoss[]{orc_1, orc_2};
+        MilitaryLoss[] tanks = new MilitaryLoss[]{tank_1, tank_2};
+        MilitaryLoss[] drones = new MilitaryLoss[]{drone_1};
+        MilitaryLoss[] missiles = new MilitaryLoss[]{missile_1};
+        MilitaryLoss[] empty = new MilitaryLoss[0];
+
+        MilitaryLoss[] actualContactedArray = TrivialArrayUtil.concatAll(
+                orcs,
+                tanks,
+                drones,
+                missiles,
+                empty);
+        MilitaryLoss[] expectedContactedArray = new MilitaryLoss[]{
+                orc_1, orc_2, tank_1, tank_2, drone_1, missile_1
+        };
+
+        assertArrayEquals(expectedContactedArray, actualContactedArray);
     }
 
 }
