c78e7a1817a156287d8e667370985ab60c817b6b
Konstantion <yagudinkostya@gmail.com>
2022-12-04 16:21:36 +0200

    L07 Written methods for tests in OddIndexIterator

--- Homework/java-core/07-Generics-Collections-Framework/src/main/java/edu/geekhub/homework/hw1/OddIndexIterator.java
+++ Homework/java-core/07-Generics-Collections-Framework/src/main/java/edu/geekhub/homework/hw1/OddIndexIterator.java
@@ -2,10 +2,12 @@ package edu.geekhub.homework.hw1;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class OddIndexIterator<E> implements Iterator<E> {
-    private List<E> data;
-    int position = 1;
+    private final List<E> data;
+    private int position = 1;
+    private static final int ODD_STEP = 2;
 
     public OddIndexIterator(List<E> data) {
         this.data = data;
@@ -13,15 +15,16 @@ public class OddIndexIterator<E> implements Iterator<E> {
 
     @Override
     public boolean hasNext() {
-        // write your code here
-        return true;
+        return position < data.size();
     }
 
     @Override
     public E next() {
-        //write your code here
-        E nextValue = null;
-        position = 0;
-        return nextValue;
+        int i = position;
+        if (i >= data.size()) {
+            throw new NoSuchElementException();
+        }
+        position += ODD_STEP;
+        return data.get(i);
     }
 }
