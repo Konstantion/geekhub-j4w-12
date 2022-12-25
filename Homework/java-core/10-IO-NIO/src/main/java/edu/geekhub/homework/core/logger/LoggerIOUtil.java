d93fe8eb54874abe93a24030da036caef081f43b
Konstantion <yagudinkostya@gmail.com>
2022-12-25 16:29:39 +0200

    L10 Added method to read/write log messages in file

---
+++ Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/core/logger/LoggerIOUtil.java
@@ -0,0 +1,100 @@
+package edu.geekhub.homework.core.logger;
+
+import java.io.*;
+import java.util.List;
+
+public class LoggerIOUtil {
+    private static final String PROJECT_DIRECTORY = System.getProperty("user.dir");
+    private static final String SEPARATOR = System.getProperty("file.separator");
+    private static final String LOG_FILE = "log.txt";
+    private boolean isFirstRecord = false;
+
+    public String buildResourcesPath() {
+        return String.join(SEPARATOR,
+                PROJECT_DIRECTORY,
+                "src",
+                "main",
+                "resources"
+        );
+    }
+
+    public String buildFullLogPath() {
+        return String.join(
+                SEPARATOR,
+                buildResourcesPath(),
+                LOG_FILE
+        );
+    }
+
+    public boolean createIfNotExist(File logFile) {
+        try {
+            return logFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void fillMessagesListFromFile(List<Message> messages) {
+        File logFile = new File(buildFullLogPath());
+        if (!createIfNotExist(logFile)) {
+            try (FileInputStream fis = new FileInputStream(buildFullLogPath())) {
+                if (fis.available() != 0) {
+                    ObjectInputStream ois = new ObjectInputStream(fis);
+                    while (fis.available() != 0) {
+                        messages.add((Message) ois.readObject());
+                    }
+                }
+            } catch (IOException | ClassCastException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        if (logFile.length() == 0) {
+            isFirstRecord = true;
+        }
+    }
+
+    public void appendMessageToFile(Message message) {
+        if (isFirstRecord) {
+            try (FileOutputStream fos = new FileOutputStream(buildFullLogPath());
+                 ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+                isFirstRecord = false;
+                oos.writeObject(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try (FileOutputStream fos = new FileOutputStream(buildFullLogPath(), true);
+                 AppendableObjectOutputStream oos = new AppendableObjectOutputStream(
+                         new ObjectOutputStream(fos))
+            ) {
+                oos.writeObject(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void saveMessages(List<Message> messages) {
+        try (FileOutputStream fos = new FileOutputStream(buildFullLogPath());
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(messages);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readMessages(List<Message> messages) {
+        try (FileInputStream fis = new FileInputStream(buildFullLogPath())) {
+            if (fis.available() != 0) {
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                messages.addAll((List<Message>) ois.readObject());
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
