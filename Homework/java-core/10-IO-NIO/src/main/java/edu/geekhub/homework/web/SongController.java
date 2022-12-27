3ccb4dd8fe7a431dfc655d30a2374c1a5654ea85
Konstantion <yagudinkostya@gmail.com>
2022-12-27 03:47:43 +0200

    L10 Added method to get all songs

--- Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/web/SongController.java
+++ Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/web/SongController.java
@@ -5,6 +5,8 @@ import edu.geekhub.homework.core.song.Song;
 import edu.geekhub.homework.core.request.Request;
 import edu.geekhub.homework.core.request.Response;
 
+import java.util.List;
+
 public class SongController {
     private final SongService service;
 
@@ -12,9 +14,15 @@ public class SongController {
         this.service = service;
     }
 
-    public Response<Object> downloadSongByUrl(Request<Song> request) {
+    public Response<Boolean> downloadSongByUrl(Request<Song> request) {
         boolean result = service.downloadSongHTTP(request.getData());
 
-        return result ? Response.ok() : Response.fail();
+        return result ? Response.ok(true) : Response.fail(false);
+    }
+
+    public Response<List<Song>> getListOfSongs() {
+        List<Song> songs = service.readAllSongsFromPlayList();
+
+        return Response.ok(songs);
     }
 }
