bab8df92762e6149dab0622bc0effb20ec55827e
Konstantion <yagudinkostya@gmail.com>
2022-12-28 00:04:00 +0200

    L10 Added InstanceInitializer

---
+++ Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/InstanceInitializer.java
@@ -0,0 +1,49 @@
+package edu.geekhub.homework;
+
+import edu.geekhub.homework.core.SongClient;
+import edu.geekhub.homework.core.SongService;
+import edu.geekhub.homework.core.logger.Logger;
+import edu.geekhub.homework.core.song.SongIOUtil;
+import edu.geekhub.homework.web.SongController;
+
+
+
+public class InstanceInitializer {
+    private SongIOUtil songIOUtil;
+    private SongClient songClient;
+    private SongService songService;
+    private SongController songController;
+    private Logger logger;
+
+    public InstanceInitializer() {
+        initialize();
+    }
+
+    private void initialize() {
+        logger = Logger.getInstance();
+        songIOUtil = new SongIOUtil();
+        songClient = new SongClient();
+        songService = new SongService(songIOUtil, songClient, logger);
+        songController = new SongController(songService);
+    }
+
+    public SongIOUtil getSongIOUtil() {
+        return songIOUtil;
+    }
+
+    public SongClient getSongClient() {
+        return songClient;
+    }
+
+    public SongService getSongService() {
+        return songService;
+    }
+
+    public SongController getSongController() {
+        return songController;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+}
