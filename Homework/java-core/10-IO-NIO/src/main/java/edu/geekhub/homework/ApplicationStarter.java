9fa531d4fc38eabf5d6c0cb1d5770434a984387e
Konstantion <yagudinkostya@gmail.com>
2022-12-28 00:05:56 +0200

    L10 Added program starter

--- Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,6 +1,11 @@
 package edu.geekhub.homework;
 
-public class ApplicationStarter {
+import edu.geekhub.homework.core.UI.UserInterface;
 
-    // Write code here :)
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        InstanceInitializer instanceInitializer = new InstanceInitializer();
+        UserInterface userInterface = new UserInterface(instanceInitializer.getLogger());
+        userInterface.drawMainMenu();
+    }
 }
