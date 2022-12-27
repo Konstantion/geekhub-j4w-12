e9a3c757c5c5900c35e154e8719857adbaa26fbc
Konstantion <yagudinkostya@gmail.com>
2022-12-28 01:42:02 +0200

    L10 Added downloading to ApplicationStarter

--- Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,10 +1,22 @@
 package edu.geekhub.homework;
 
 import edu.geekhub.homework.core.UI.UserInterface;
+import edu.geekhub.homework.core.request.Request;
+import edu.geekhub.homework.core.song.Song;
+import edu.geekhub.homework.web.SongController;
+
+import java.util.List;
 
 public class ApplicationStarter {
     public static void main(String[] args) {
         InstanceInitializer instanceInitializer = new InstanceInitializer();
+        SongController controller = instanceInitializer.getSongController();
+        List<Song> songs = controller.getListOfSongs().getData();
+
+        songs.stream()
+                .map(Request::new)
+                .forEach(controller::downloadSongByUrl);
+
         UserInterface userInterface = new UserInterface(instanceInitializer.getLogger());
         userInterface.drawMainMenu();
     }
