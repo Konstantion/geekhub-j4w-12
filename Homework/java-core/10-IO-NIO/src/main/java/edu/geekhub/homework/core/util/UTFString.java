b5d3912683eacfb8b5a4748c5a4d232bb49d1ebc
Konstantion <yagudinkostya@gmail.com>
2022-12-27 02:36:02 +0200

    L10 Fixed UTFString

--- Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/core/util/UTFString.java
+++ Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/core/util/UTFString.java
@@ -1,17 +1,53 @@
 package edu.geekhub.homework.core.util;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class UTFString {
-    private UTFString() {
+    private static final String PROBE = StandardCharsets.UTF_8.name();
+    private static final String[] CHARSETS = new String[]
+            {"windows-1251", "ISO-8859-1", "UTF-8", "UTF-16", "US_ASCII"};
 
+    private UTFString() {
     }
 
     public static String of(String string) {
+        String charset = charset(string);
+
+        if (!charset.equals(PROBE)) {
+            string = convertToUTF(string, charset);
+        }
+
+        return string;
+    }
+
+    private static String charset(String value, String[] charsets) {
+        for (String c : charsets) {
+            Charset charset = Charset.forName(c);
+            if (value.equals(convert(convert(value, charset.name(), PROBE), PROBE, charset.name()))) {
+                return c;
+            }
+        }
+        return StandardCharsets.UTF_8.name();
+    }
+
+    private static String charset(String value) {
+        return charset(value, CHARSETS);
+    }
+
+    private static String convert(String value, String fromEncoding, String toEncoding) {
         try {
-            byte[] bytes = string.getBytes("windows-1251");
-            return new String(bytes, StandardCharsets.UTF_8);
+            return new String(value.getBytes(fromEncoding), toEncoding);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private static String convertToUTF(String value, String fromEncoding) {
+        try {
+            return new String(value.getBytes(fromEncoding), StandardCharsets.UTF_8);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
