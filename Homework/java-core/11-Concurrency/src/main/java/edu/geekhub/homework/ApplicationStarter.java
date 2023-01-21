f91c69348d7faee067ebd595a8be8ae9ec112eda
Konstantion <yagudinkostya@gmail.com>
2023-01-21 02:41:48 +0200

    L11 remove unnecessary lines in main

--- Homework/java-core/11-Concurrency/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/11-Concurrency/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -3,8 +3,7 @@ package edu.geekhub.homework;
 public class ApplicationStarter {
 
     public static void main(String[] args) {
-        RatRace ratRace = new RatRace(40);
-        String field = ratRace.getGameFieldAsString();
-        System.out.println(field);
+        RatRace ratRace = new RatRace(10);
     }
+
 }
