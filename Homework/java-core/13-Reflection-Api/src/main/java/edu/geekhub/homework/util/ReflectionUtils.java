fdf4921038c621c3b0fecfc9a8e71aa8c5dae8b3
Konstantion <yagudinkostya@gmail.com>
2023-02-05 01:36:38 +0200

    L12 added reflection utils to work with reflection

---
+++ Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/util/ReflectionUtils.java
@@ -0,0 +1,54 @@
+package edu.geekhub.homework.util;
+
+import edu.geekhub.homework.exceptions.PropertiesFormatException;
+
+import java.lang.reflect.Field;
+
+import static java.lang.String.format;
+
+public class ReflectionUtils {
+    private ReflectionUtils() {
+
+    }
+
+    public static boolean isFieldOfType(Field field, Class<?> type) {
+        return field.getType().equals(type);
+    }
+
+    public static boolean isFieldOfType(Field field, Object object) {
+        return isFieldOfType(field, object.getClass());
+    }
+
+    public static boolean isFieldIntegerOrInt(Field field) {
+        return isFieldOfType(field, Integer.class) || isFieldOfType(field, int.class);
+    }
+
+    public static boolean isFieldEnum(Field field) {
+        return field.getType().isEnum();
+    }
+
+    public static boolean isObjectCanBeParsedToInteger(Object obj) throws PropertiesFormatException {
+        try {
+            Integer.parseInt(obj.toString());
+            return true;
+        } catch (NumberFormatException e) {
+            throw new PropertiesFormatException(
+                    format("Can't convert value %s, to the Integer", obj)
+            );
+        }
+    }
+
+    public static <T extends Enum<T>> T tryToParseObjectToEnum(Object obj, Class<T> enumClass) throws PropertiesFormatException {
+        try {
+            return Enum.valueOf(enumClass, obj.toString());
+        } catch (Exception e) {
+            throw new PropertiesFormatException(
+                    format("Can't convert value %s, to %s, %s",
+                            obj,
+                            enumClass.getName(),
+                            e.getMessage()
+                    )
+            );
+        }
+    }
+}
