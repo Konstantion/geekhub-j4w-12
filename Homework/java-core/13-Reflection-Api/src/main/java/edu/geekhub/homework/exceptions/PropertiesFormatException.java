eb3e84e44ae6d7cffd617fd4e28d4d56ba439a26
Konstantion <yagudinkostya@gmail.com>
2023-02-05 01:36:17 +0200

    L12 added custom exception

---
+++ Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/exceptions/PropertiesFormatException.java
@@ -0,0 +1,7 @@
+package edu.geekhub.homework.exceptions;
+
+public class PropertiesFormatException extends Exception{
+    public PropertiesFormatException(String message) {
+        super(message);
+    }
+}
---
+++ Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/exceptions/PropertiesNotFoundException.java
@@ -0,0 +1,7 @@
+package edu.geekhub.homework.exceptions;
+
+public class PropertiesNotFoundException extends RuntimeException{
+    public PropertiesNotFoundException(String message) {
+        super(message);
+    }
+}
