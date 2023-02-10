e3ee78eb0eee2c55eee8c75ae7c7bcf75ee037c4
Konstantion <yagudinkostya@gmail.com>
2023-02-10 23:17:56 +0200

    L14 added id to Product

--- Homework/java-web/domain/src/main/java/product/Product.java
+++ Homework/java-web/domain/src/main/java/product/Product.java
@@ -1,15 +1,17 @@
-package entity;
+package product;
 
 import java.util.Objects;
 
 public class Product {
+    private Integer id;
     private String name;
     private Integer price;
 
     public Product() {
     }
 
-    public Product(String name, Integer price) {
+    public Product(Integer id, String name, Integer price) {
+        this.id = id;
         this.name = name;
         this.price = price;
     }
@@ -30,27 +32,29 @@ public class Product {
         this.price = price;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Product product)) return false;
-        return Objects.equals(name, product.name) && Objects.equals(price, product.price);
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(price, product.price);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, price);
+        return Objects.hash(id, name, price);
     }
 
-    @Override
-    public String toString() {
-        return "Product{" +
-               "name='" + name + '\'' +
-               ", price=" + price +
-               '}';
-    }
 
     public static final class ProductBuilder {
+        private Integer id;
         private String name;
         private Integer price;
 
@@ -59,6 +63,11 @@ public class Product {
 
         public static ProductBuilder builder() {
             return new ProductBuilder();
+        }
+
+        public ProductBuilder id(Integer id) {
+            this.id = id;
+            return this;
         }
 
         public ProductBuilder name(String name) {
@@ -73,6 +82,7 @@ public class Product {
 
         public Product build() {
             Product product = new Product();
+            product.setId(id);
             product.setName(name);
             product.setPrice(price);
             return product;
