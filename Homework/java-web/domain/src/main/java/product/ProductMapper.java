fa1e35a72dd03b3a18e58e471aa0852c80b91771
Konstantion <yagudinkostya@gmail.com>
2023-02-13 03:50:59 +0200

    L14 added method to map list to mapper

--- Homework/java-web/domain/src/main/java/product/ProductMapper.java
+++ Homework/java-web/domain/src/main/java/product/ProductMapper.java
@@ -5,11 +5,15 @@ import org.mapstruct.factory.Mappers;
 import product.dto.CreationProductDto;
 import product.dto.ProductDto;
 
+import java.util.List;
+
 @Mapper
 public interface ProductMapper {
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
     ProductDto toDto(Product entity);
+
+    List<ProductDto> toDto(List<Product> entity);
 
     Product toEntity(CreationProductDto dto);
 }
