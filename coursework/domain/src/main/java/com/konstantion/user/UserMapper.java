28b65e3e3c088e78c05c26bbdcddcdff15775c84
Konstantion <yagudinkostya@gmail.com>
2023-03-13 23:47:32 +0200

    coursework added user dto and mapper

---
+++ coursework/domain/src/main/java/com/konstantion/user/UserMapper.java
@@ -0,0 +1,12 @@
+package com.konstantion.user;
+
+import com.konstantion.user.dto.UserDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDto toDto(User user);
+}
