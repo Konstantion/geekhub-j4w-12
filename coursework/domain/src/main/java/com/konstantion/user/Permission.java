48efc3575d345d2e7eb6d25a339cf12b1a749444
Konstantion <yagudinkostya@gmail.com>
2023-03-13 23:49:03 +0200

    coursework added util method to user role and permission

--- coursework/domain/src/main/java/com/konstantion/user/Permission.java
+++ coursework/domain/src/main/java/com/konstantion/user/Permission.java
@@ -1,5 +1,7 @@
 package com.konstantion.user;
 
+import java.util.Set;
+
 public enum Permission {
     CREATE_TABLE, DELETE_TABLE,
     CREATE_PRODUCT, DELETE_PRODUCT, EDIT_PRODUCT,
@@ -9,5 +11,9 @@ public enum Permission {
     CREATE_BILL_FROM_TABLE,
     CLOSE_BILL, ACTIVATE_BILL, GET_BILL,
     CLOSE_CALL, CALL_WAITER,
-    TRANSFER_ORDER
+    TRANSFER_ORDER;
+
+    public static Set<Permission> getDefaultTablePermissions() {
+        return Set.of(CALL_WAITER, GET_BILL);
+    }
 }
--- coursework/domain/src/main/java/com/konstantion/user/Role.java
+++ coursework/domain/src/main/java/com/konstantion/user/Role.java
@@ -1,7 +1,13 @@
 package com.konstantion.user;
 
+import java.util.Set;
+
 public enum Role {
     ADMIN,
     WAITER,
-    TABLE
+    TABLE;
+
+    public static Set<Role> getTableRole() {
+        return Set.of(TABLE);
+    }
 }
