3531a23b39fd9e8ab9c494a9e421449b360f8de0
Konstantion <yagudinkostya@gmail.com>
2023-03-13 23:46:51 +0200

    coursework modified table

--- coursework/domain/src/main/java/com/konstantion/table/Table.java
+++ coursework/domain/src/main/java/com/konstantion/table/Table.java
@@ -1,91 +1,63 @@
 package com.konstantion.table;
 
+import com.konstantion.hall.Hall;
 import com.konstantion.order.Order;
 import com.konstantion.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 import static java.util.Objects.nonNull;
 
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@jakarta.persistence.Table(name = "table", schema = "public")
 public class Table {
-    private Long id;
-    private UUID uuid;
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
     private String name;
     private Integer capacity;
     private TableType tableType;
+
+    @ManyToOne
+    private Hall hall;
+    @OneToOne
     private Order order;
+    @OneToOne
+    private User tableUser;
+
+    @ManyToMany
+    @JoinTable(
+            name = "table_waiter",
+            joinColumns = @JoinColumn(name = "table_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
     private List<User> waiters;
     private Boolean active;
+    private LocalDateTime createdAt;
+
+    private LocalDateTime deletedAt;
 
     public Table() {
     }
 
-    public boolean isOccupied() {
+    public boolean hasOrder() {
         return nonNull(order);
     }
 
-    public static TableBuilder builder() {
-        return new TableBuilder();
-    }
+    public boolean hasWaiters() {return waiters.isEmpty();}
 
-    public static final class TableBuilder {
-        private UUID uuid;
-        private String name;
-        private Integer capacity;
-        private TableType tableType;
-        private Order order;
-        private List<User> waiters;
-        private Boolean active;
-
-        private TableBuilder() {
-        }
-
-        public TableBuilder uuid(UUID uuid) {
-            this.uuid = uuid;
-            return this;
-        }
-
-        public TableBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public TableBuilder capacity(Integer capacity) {
-            this.capacity = capacity;
-            return this;
-        }
-
-        public TableBuilder tableType(TableType tableType) {
-            this.tableType = tableType;
-            return this;
-        }
-
-        public TableBuilder order(Order order) {
-            this.order = order;
-            return this;
-        }
-
-        public TableBuilder waiters(List<User> waiters) {
-            this.waiters = waiters;
-            return this;
-        }
-
-        public TableBuilder active(Boolean active) {
-            this.active = active;
-            return this;
-        }
-
-        public Table build() {
-            Table table = new Table();
-            table.name = this.name;
-            table.tableType = this.tableType;
-            table.capacity = this.capacity;
-            table.waiters = this.waiters;
-            table.order = this.order;
-            table.active = this.active;
-            table.uuid = this.uuid;
-            return table;
-        }
-    }
+    public boolean isActive() {return active;}
 }
